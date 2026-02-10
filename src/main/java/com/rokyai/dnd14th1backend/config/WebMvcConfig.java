dbe19ad3f427b53964c40da16197c07090d15a3c
rookedsysc <rookedsysc36@gmail.com>
2026-02-10 19:34:53 +0900

    config: resource register

--- src/main/java/com/rokyai/dnd14th1backend/config/WebMvcConfig.java
+++ src/main/java/com/rokyai/dnd14th1backend/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ApiVersionConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -17,6 +18,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
         configurer
                 .useRequestHeader(ApiConstants.API_VERSION_HEADER)
                 .setDefaultVersion(ApiConstants.DEFAULT_API_VERSION);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/badge/**").addResourceLocations("classpath:/static/badge/");
     }
 
     @Override
